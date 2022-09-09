6418dae504f96cc751ec54a655ac14cd49973d59
Akim <gentilakili98@gmail.com>
2022-09-09 23:21:34 +0200

    Add instance

---
+++ src/instance/InstanceModel.js
@@ -0,0 +1,2 @@
+import { Sequelize,DataTypes } from "sequelize";    
+
---
+++ src/model/index.js
@@ -0,0 +1,36 @@
+import agent from "./ModelAgent.js";
+import client from "./ModelClient.js";
+import user from "./ModelUtilisateur.js";
+import compte from "./ModelCompte.js";
+import cateCompte from "./ModelCateCompte.js";
+import categories from "./ModelCategorie.js";
+import detailFournir from "./ModelDetailFournir.js";
+import enteteFournir from "./ModelEnteteFournir.js";
+import enteteFacture from "./ModelEnteteFacture.js";
+import detailFacture from "./ModelFacture.js";
+import entreprises from "./Modelentreprise.js";
+import fournisseur from "./ModelFournisseur.js";
+import operations from "./ModelOperation.js";
+import paiement from "./ModelPaiement.js";
+import produit from "./ModelProduit.js";
+import stock from "./ModelStock.js";
+import taux from "./ModelTaux.js";
+export default {
+  agent,
+  client,
+  user,
+  compte,
+  cateCompte,
+  categories,
+  detailFacture,
+  detailFournir,
+  enteteFacture,
+  enteteFournir,
+  entreprises,
+  fournisseur,
+  operations,
+  paiement,
+  produit,
+  stock,
+  taux,
+};
